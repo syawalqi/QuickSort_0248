7a64c5d5cf1e042f2ead34c35e98930096468cdf
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-28 14:57:33 +0700

    membaut sistem swap index

--- quicksort.cpp
+++ quicksort.cpp
@@ -57,6 +57,13 @@ void q_short(int low, int high) {
             j--; //step 8
             cmp_count++;
         }
+         cmp_count++;
+        if (i < j) //step 9
+        {
+            //swap the element at index i whit the element at index j 
+            swap(i, j);
+        }
+
 
 }
 
