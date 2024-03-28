465f8508114baaa55c0107329fe58f4add012980
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-28 14:56:29 +0700

    membuat while statement

--- quicksort.cpp
+++ quicksort.cpp
@@ -50,7 +50,9 @@ void q_short(int low, int high) {
             i++; //step 6
             cmp_count++;
         }
-
+         cmp_count++;
+        //search for an element Less than or equal to pivot 
+        while ((arr[j] > pivot) && (j >= low)) //step 7
 
 }
 
