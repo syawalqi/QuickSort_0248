d6aefb2d147ed01ba31e575808174753cd7f482b
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-28 14:55:48 +0700

    membuat while statement

--- quicksort.cpp
+++ quicksort.cpp
@@ -42,6 +42,15 @@ void q_short(int low, int high) {
      pivot = arr[low]; //step 2
     i = low + 1; //step 3
     j = high; //step 4
+      while (i <= j) //step 10
+    {
+        //search for an element greater than pivot
+        while ((arr[i] <= pivot) && (i <= high)) // step 5
+        {
+            i++; //step 6
+            cmp_count++;
+        }
+
 
 }
 
