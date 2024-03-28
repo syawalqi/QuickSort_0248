48d72fc5f2322d72036b8a55752e3308c5347f2d
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-28 14:58:57 +0700

    menambahkan sistem swap index

--- quicksort.cpp
+++ quicksort.cpp
@@ -63,7 +63,15 @@ void q_short(int low, int high) {
             //swap the element at index i whit the element at index j 
             swap(i, j);
         }
-
+         }
+    if (low < j) { //step 11
+        //swap the pivot element with the element at index j 
+        swap(low, j);
+    }
+    //recursive call to sort the left sub array
+    q_short(low, j - 1); //step 12
+    //recursive call to sort the right sub array
+    q_short(j + 1, high); //step 13
 
 }
 
