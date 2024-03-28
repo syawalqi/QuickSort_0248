8c6c0ebad9f6f1550c3c56c5db07adb3d887a402
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-28 15:00:47 +0700

    menambahkan int main

--- quicksort.cpp
+++ quicksort.cpp
@@ -87,6 +87,23 @@ void display()
         cout << "Number of data movements:" << mov_count << endl;
 }
 
+int main()
+{
+    char ch;
+    do {
+        input();
+        q_short(0, n - 1);
+        display();
+        cout << "\n\nDo you want to continue? (y/n): ";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            break;
+        system("pause");
+        system("cls");
+    } while (true);
+return 0;;
+}
+
 
 
 
