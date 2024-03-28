6a21732d5d759e18a9772bd882542c57e82ae7a1
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-28 14:51:07 +0700

    membuat pivot

--- quicksort.cpp
+++ quicksort.cpp
@@ -33,6 +33,12 @@ void swap(int x, int y){
     mov_count++; 
 }
 
-void q_short(int low, int high)
+void q_short(int low, int high) {
+     int temp;
+    int pivot, i, j;
+    if (low > high) { //step 1
+        return;
+    }
+}
 
 
