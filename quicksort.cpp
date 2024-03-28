d8468b15afed3a8adf2adc0e9a50ef45cfdac377
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-28 14:57:01 +0700

    membuat sistem sorting

--- quicksort.cpp
+++ quicksort.cpp
@@ -53,6 +53,10 @@ void q_short(int low, int high) {
          cmp_count++;
         //search for an element Less than or equal to pivot 
         while ((arr[j] > pivot) && (j >= low)) //step 7
+          {
+            j--; //step 8
+            cmp_count++;
+        }
 
 }
 
